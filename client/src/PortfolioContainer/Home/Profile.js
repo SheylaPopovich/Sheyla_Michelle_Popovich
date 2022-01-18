075ba87183bdcc598e7c1481ba0b1ae7b3508169
Sheyla <Sheylapopovich@gmail.com>
2022-01-17 20:01:54 -0700

    created divs for profile picture

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -89,6 +89,9 @@ export default function Profile() {
             </a>
           </div>
         </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
       </div>
     </div>
   );
