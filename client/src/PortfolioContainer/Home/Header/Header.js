e83bcae9e072f7db39f1d94bbba2aeb9e4b49345
Sheyla <Sheylapopovich@gmail.com>
2022-01-21 10:59:03 -0700

    imported fabBars and FontAwesome Icon onto Header JS file

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {TOTAL_SCREENS, GET_SCREEN_INDEX} from "../../../utilities/commonUtils";
 import ScrollService from "../../../utilities/ScrollService";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return <div>
