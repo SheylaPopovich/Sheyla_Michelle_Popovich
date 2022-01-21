3277baa12dcce87cbad7d4fb4cc1559f674a36ca
Sheyla <Sheylapopovich@gmail.com>
2022-01-21 12:25:52 -0700

    hamburger menue finalized

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -51,15 +51,24 @@ export default function Header() {
   };
 
   return (
-<div className="header-option" onClick={() => setShowHeaderOptions(!showHeaderOptions) }>
-    <div className="header-parent">
-        <div className="header-hamburger" onClick={() => setShowHeaderOptions(!showHeaderOptions)}>
-          <FontAwesomeIcon className="header-hamburger-bars" icon={faBars}/>  
+    <div
+      className="header-option"
+      onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+    >
+      <div className="header-parent">
+        <div
+          className="header-hamburger"
+          onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+        >
+          <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
         </div>
         <div className="header-logo">
-            <span>SHEYLA</span>
+          <span>SHEYLA~</span>
         </div>
+        <div className={(showHeaderOptions) ? "header-options show-hamburger-options": "header-options"} >
+            {getHeaderOptions()}
+        </div>
+      </div>
     </div>
-</div>
-  ) 
+  );
 }
