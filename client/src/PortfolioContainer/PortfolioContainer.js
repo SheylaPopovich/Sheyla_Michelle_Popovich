c816c5ff2949be3686720d2bdc51dc4c05585b99
Sheyla <Sheylapopovich@gmail.com>
2022-01-20 19:43:17 -0700

    returned portfolioContainer function

--- client/src/PortfolioContainer/PortfolioContainer.js
+++ client/src/PortfolioContainer/PortfolioContainer.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { TOTAL_SCREENS } from '../utilities/commonUtils';
+
 
 export default function PortfolioContainer() {
-  return <div></div>;
+   
+    const mapAllScreens = ()=>{
+        return(
+            TOTAL_SCREENS.map((screen)=>(
+                (screen.component) ? <screen.component screenName={screen.screen_name}
+                id={screen.screen_name}/> : <div key={screen.screen_name}></div>
+            ))
+        )
+    }
+
+  return (
+       <div className='portfolio-container'>
+{map}
+  </div>
+  )
 }
