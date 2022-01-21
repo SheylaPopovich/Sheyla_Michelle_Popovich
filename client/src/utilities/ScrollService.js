7eaced38ae448a0879d52cc665fa4bc8b89f75cb
Sheyla <Sheylapopovich@gmail.com>
2022-01-20 20:18:14 -0700

    created constructor on ScrollService js file

--- client/src/utilities/ScrollService.js
+++ client/src/utilities/ScrollService.js
@@ -1,8 +1,24 @@
 import {TOTAL_SCREENS} from './commonUtils';
-import {subject} from "rxjs"
+import {Subject, subject} from "rxjs"
 
 export default class ScrollService{
     static scrollHandler = new ScrollService();
 
+    static currentScreenBroadCaster = new Subject()
+    static currentScreenFadeIn = new Subject()
 
+
+    constructor(){
+        window.addEventListener('scroll', this.checkCurrentScreenUnderViewPort);
+    }
+    scrollToHireMe = ()=>{
+        let contactMeScreen = document.getElementById("Contact Me")
+        if(!contactMeScreen) return;
+        contactMeScreen.scrollIntoView({behavior: "smooth"})
+    }
+    scrollToHome = ()=>{
+        let contactMeScreen = document.getElementById("Home")
+        if(!contactMeScreen) return;
+        contactMeScreen.scrollIntoView({behavior: "smooth"})
+    }
 }
