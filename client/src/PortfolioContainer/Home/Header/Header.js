9faed5fe17c3152dffb3974b811097182295fff4
Sheyla <Sheylapopovich@gmail.com>
2022-01-21 08:53:22 -0700

    imported total_screens and get_screen_index onto header js file

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {TOTAL_SCREENS, GET_SCREEN_INDEX} from "../../../utilities/commonUtils"
+
 
 export default function Header() {
   return <div>
-      
+
   </div>;
 }
