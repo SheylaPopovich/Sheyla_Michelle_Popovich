cd205647c9c7bac7a5fb2374205325c5b0912ac3
Sheyla <Sheylapopovich@gmail.com>
2022-01-21 08:54:30 -0700

    imported scrollService from utilities folder onto header js file

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {TOTAL_SCREENS, GET_SCREEN_INDEX} from "../../../utilities/commonUtils"
-
+import {TOTAL_SCREENS, GET_SCREEN_INDEX} from "../../../utilities/commonUtils";
+import ScrollService from "../../../utilities/ScrollService";
 
 export default function Header() {
   return <div>
