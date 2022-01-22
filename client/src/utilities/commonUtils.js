6d0e4809ae4cd07f878833fcf1000948aef2edbb
Sheyla <Sheylapopovich@gmail.com>
2022-01-22 15:08:24 -0700

    added key pair value for resume on commonUtils js file

--- client/src/utilities/commonUtils.js
+++ client/src/utilities/commonUtils.js
@@ -11,6 +11,10 @@ export const TOTAL_SCREENS = [
 {
     screen_name: "AboutMe",
     component: AboutMe,
+},
+{
+    screen_name: "Resume",
+    component: Resume,
 }
 ]
 export const GET_SCREEN_INDEX = (screen_name) =>{
