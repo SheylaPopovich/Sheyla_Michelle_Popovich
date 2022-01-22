a816766ea5ebc373c8bbd11949c001d3a2299b26
Sheyla <Sheylapopovich@gmail.com>
2022-01-22 10:06:22 -0700

    imported srollService onto aboutMe

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from "react";
-import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading"
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+
 
 export default function AboutMe() {
   return <div></div>;
