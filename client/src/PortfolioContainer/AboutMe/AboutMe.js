c539c436690d0407a303ad0a250394a61900aece
Sheyla <Sheylapopovich@gmail.com>
2022-01-22 11:15:47 -0700

    added buttons onto aboutMe js file

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -11,22 +11,55 @@ export default function AboutMe(props) {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-    const SCREEN_CONSTANTS = {
-        description: "Zoox’s fleet management application is an essential tool in the operation of our ever-growing fleet of autonomous robotaxis. We build tools to interact with and monitor our vehicles remotely",
-        highlight: {
-            bullets: [
-                "I can do this",
-                "I can do that",
-                "But I really dont know what Im doing",
-            ],
-            heading: "Here are a Few Highlights:"
-        }
-    }
+  const SCREEN_CONSTANTS = {
+    description:
+      "Zoox’s fleet management application is an essential tool in the operation of our ever-growing fleet of autonomous robotaxis. We build tools to interact with and monitor our vehicles remotely",
+    highlights: {
+      bullets: [
+        "I can do this",
+        "I can do that",
+        "But I really dont know what Im doing",
+      ],
+      heading: "Here are a Few Highlights:",
+    },
+  };
+
+  const renderHighlights = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
 
   return (
     <div className="about-me-container screen-container" id={props.id || ""}>
       <div className="about-me-parent">
         <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighlights()}
+            </div>
+            <div className="about-me-options">
+              <button className="btn primary-btn"> Hire Me </button>
+              <a
+                href="Sheyla_Popovich_Resume.pdf"
+                download="Sheyla_Popovich_Resume.pdf "
+              >
+                <button className="btn highlighted-btn"> Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
