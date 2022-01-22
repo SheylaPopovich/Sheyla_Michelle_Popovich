3c1e2e9c6a67d6c1bc684b04f4faa93150496097
Sheyla <Sheylapopovich@gmail.com>
2022-01-22 10:30:52 -0700

    added screenHeading component onto aboutMe funtion to render

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -3,6 +3,12 @@ import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
 
-export default function AboutMe() {
-  return <div></div>;
+export default function AboutMe(props) {
+  return ( 
+  <div className="about-me-container screen-container">
+      <div className="about-me-parent">
+    <ScreenHeading title={'About Me'} subHeading={'Why Choose Me'} />
+      </div>
+  </div>
+  )
 }
