af9c395a6b3f04297e311c9d1577dc11e7c22b19
Sheyla <Sheylapopovich@gmail.com>
2022-01-22 15:00:26 -0700

    imported Animations into resume component

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
-
+import Animations from '../../utilities/Animations';
 
 
 
