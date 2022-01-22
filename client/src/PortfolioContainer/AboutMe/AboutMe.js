fd1bc1617398dbbaabcd480d32c305fc34f2a2c9
Sheyla <Sheylapopovich@gmail.com>
2022-01-22 09:57:37 -0700

    imported screenHeading onto aboutMe js file

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading"
 
 export default function AboutMe() {
   return <div></div>;
