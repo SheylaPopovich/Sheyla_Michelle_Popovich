0ed397720ddd7840571f87244340ecd27715a841
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 16:11:47 -0700

    imported Testimonial component onto commonUtils file

--- client/src/utilities/commonUtils.js
+++ client/src/utilities/commonUtils.js
@@ -1,6 +1,7 @@
 import Home from "../PortfolioContainer/Home/Home";
 import AboutMe from "../PortfolioContainer/AboutMe/AboutMe";
 import Resume from "../PortfolioContainer/Resume/Resume";
+import Testimonial from "../PortfolioContainer/Testimonial/Testimonial";
 
 export const TOTAL_SCREENS = [
   {
