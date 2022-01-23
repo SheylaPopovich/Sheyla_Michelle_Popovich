41114f52695a52c67edbd84ac233279993b4b368
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 16:04:52 -0700

    imported react owl style sheets onto testimonial js file

--- client/src/PortfolioContainer/Testimonial/Testimonial.js
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+
 
 export default function Testimonial() {
   return (
