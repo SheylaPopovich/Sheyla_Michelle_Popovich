bb0c13f3ddd3da8f535914af683602122f0ba694
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 16:06:48 -0700

    imported Animations to Testimonial js

--- client/src/PortfolioContainer/Testimonial/Testimonial.js
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -6,7 +6,7 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from  "../../utilities/ScrollService";
-
+import Animations from '../../utilities/Animations';
 
 
 export default function Testimonial() {
