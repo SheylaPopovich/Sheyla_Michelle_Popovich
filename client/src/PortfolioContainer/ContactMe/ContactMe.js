8f6c47d81070a357b0124c3dff5152aba87a5c7f
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 19:37:38 -0700

    fixed typo on contactMe classname central-form

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -34,14 +34,14 @@ export default function ContactMe(props) {
 
   return (
     <div className="main-container" id={props.id || ""}>
-      <ScreenHeading subHeading={"Let's keep in Touch"} title={"Contact Me"} />
-      <div className="central-from">
+      <ScreenHeading subHeading={"Let's keep in Touch"}  title={props.screenName ? props.screenName : ""}/>
+      <div className="central-form">
         <div className="col">
           <h2 className="title">
             {" "}
             <Typical
               loop={Infinity}
-              steps={["Get In Touch 📧 :email:", 1000]}
+              steps={["Get In Touch 📧", 1000]}
             />
           </h2>
           <a
@@ -73,19 +73,21 @@ export default function ContactMe(props) {
             <i className="fa fa-github-square"></i>
           </a>
         </div>
+
         <div className="back-form">
           <div className="img-back">
             <h4>Send Your Email Here!</h4>
             <img src={imgBack} alt="image not found" />
           </div>
-          <form>
+          <form >
             <p>{banner}</p>
             <label htmlFor="name">Name</label>
             <input type="text" onChange={handleName} value={name} />
             <label htmlFor="email">Email</label>
             <input type="email" onChange={handleEmail} value={email} />
             <label htmlFor="message">Message</label>
-            <textarea type="text" onChange={handleMessage} value={message} />
+            <textarea type="text" onChange={handleMessage} value={message} name="message" />
+
             <div className="send-btn">
               <button type="submit">
                 Send <i className="fa fa-paper-plane" />
