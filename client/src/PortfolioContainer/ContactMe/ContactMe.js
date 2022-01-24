ff1d9bfd324e81cc2eed0c9f1b7b5fd6b2e974ed
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 18:41:10 -0700

    added image onto contactMe form

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Typical from "react-typical";
 
 import imgBack from "../../../src/images/mailz.jpeg";
@@ -6,6 +6,7 @@ import load1 from "../../../src/images/load2.gif";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
+import { useState } from "react";
 
 export default function ContactMe(props) {
   let fadeInScreenHandler = (screen) => {
@@ -14,6 +15,12 @@ export default function ContactMe(props) {
   };
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBoolean] = useState(false);
 
   return (
     <div className="main-container" id={props.id || ""}>
@@ -28,33 +35,39 @@ export default function ContactMe(props) {
             />
           </h2>
           <a
-                aria
-                label="link to facebook "
-                href="https://www.facebook.com/sheyla.michelle.303"
-              >
-                <i className="fa fa-facebook-square"></i>
-              </a>
-              <a
-                aria
-                label="link to instagram"
-                href="https://www.instagram.com/sheyla_popovich/"
-              >
-                <i className="fa fa-instagram"></i>
-              </a>
-              <a
-                aria
-                label="link to linkedin"
-                href="https://www.linkedin.com/in/sheyla-popovich-fsd/"
-              >
-                <i className="fa fa-linkedin-square"></i>
-              </a>
-              <a
-                aria
-                label="link to github"
-                href="https://github.com/SheylaPopovich"
-              >
-                <i className="fa fa-github-square"></i>
-              </a>
+            aria
+            label="link to facebook "
+            href="https://www.facebook.com/sheyla.michelle.303"
+          >
+            <i className="fa fa-facebook-square"></i>
+          </a>
+          <a
+            aria
+            label="link to instagram"
+            href="https://www.instagram.com/sheyla_popovich/"
+          >
+            <i className="fa fa-instagram"></i>
+          </a>
+          <a
+            aria
+            label="link to linkedin"
+            href="https://www.linkedin.com/in/sheyla-popovich-fsd/"
+          >
+            <i className="fa fa-linkedin-square"></i>
+          </a>
+          <a
+            aria
+            label="link to github"
+            href="https://github.com/SheylaPopovich"
+          >
+            <i className="fa fa-github-square"></i>
+          </a>
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            <img src={imgBack} alt="image not found" />
+          </div>
         </div>
       </div>
     </div>
