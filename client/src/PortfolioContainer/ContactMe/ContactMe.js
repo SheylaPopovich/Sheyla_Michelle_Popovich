57e690e72f892e5993e667d22e954a1493ec2cb3
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 18:13:02 -0700

    imported contactMe onto commonUtils js file

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -6,7 +6,18 @@ import ScrollService from "../../utilities/ScrollService/";
 import Animations from "../../utilities/Animations";
 
 
-export default function ContactMe() {
-  return (<div></div>)
+export default function ContactMe(props) {
+    
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id);
+      };
+      const fadeInSubscription =
+        ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+    
+
+  return (
+  <div></div>
+  )
   
 }
