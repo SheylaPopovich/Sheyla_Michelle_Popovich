9d30993a15417a5dc2956d76933277478cb5610c
Sheyla <Sheylapopovich@gmail.com>
2022-01-23 18:02:09 -0700

    imported all necessary component onto contactMe js file

---
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import imgBack from "../../../src/images/mailz.jpeg";
+import load1 from "../../../src/images/load2.gif";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService/";
+import Animations from "../../utilities/Animations";
+
+
+export default function ContactMe() {
+  return (<div></div>)
+  
+}
