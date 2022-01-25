bc2c7b73bd28121aa6b60db5c6588c4e707fdb56
Sheyla <Sheylapopovich@gmail.com>
2022-01-24 20:33:10 -0700

    app use on for express json

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config
 const express = require("express")
 const cors = require("cors")
-const path = require("path")
+const path = require("path");
+const { asapScheduler } = require("rxjs");
 
 const app = express();
+
+app.use(express.json())
