493aab00f918f5f2c0b8ecc83e186fc33b40519f
Sheyla <Sheylapopovich@gmail.com>
2022-01-25 11:29:04 -0700

    added parathensis to require dotenv config on server js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-require("dotenv").config
-const express = require("express")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
-const contactRoute = require('./route/contactRoute')
+const contactRoute = require("./route/contactRoute");
 
 const app = express();
 
-
 //creating the middleware
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`server listing to port 5000`))
+app.listen(port, console.log(`server listing to port 5000`));
