35cfd58a73478984345f0fdea168cafac3b02f2e
Sheyla <Sheylapopovich@gmail.com>
2022-01-25 09:23:30 -0700

    returned router post function with json message

--- route/contactRoute.js
+++ route/contactRoute.js
@@ -5,5 +5,7 @@ const nodemailer = require('nodemailer')
 
 router.post('/contact', (req,res)=> {
     let data = req.body
-    if (data.name.length === 0 || data.email.length === 0 || data.message.length === 0) {}
+    if (data.name.length === 0 || data.email.length === 0 || data.message.length === 0) {
+        return res.json({msg: "please fill all fields"})
+    }
 })
