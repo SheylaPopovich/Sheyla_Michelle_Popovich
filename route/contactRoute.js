956e458f677a000388fce84db7f3037264b02e1d
Sheyla <Sheylapopovich@gmail.com>
2022-01-25 11:27:31 -0700

    required contactRoute to server js

--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,27 +1,30 @@
-const router = require('express').Router()
-const req = require('express/lib/request')
-const nodemailer = require('nodemailer')
+const router = require("express").Router();
+const req = require("express/lib/request");
+const nodemailer = require("nodemailer");
 
+router.post("/contact", (req, res) => {
+  let data = req.body;
+  if (
+    data.name.length === 0 ||
+    data.email.length === 0 ||
+    data.message.length === 0
+  ) {
+    return res.json({ msg: "please fill all fields" });
+  }
 
-router.post('/contact', (req,res)=> {
-    let data = req.body
-    if (data.name.length === 0 || data.email.length === 0 || data.message.length === 0) {
-        return res.json({msg: "please fill all fields"})
-    }
-
-        let smtpTransporter = nodemailer.createTransport({
-            service: 'Gmail',
-            port: 465,
-            auth:{
-                user: 'sheylapopovich@gmail.com',
-                pass:'Jamie2019!'
-            }
-        })
-        let mailOptions = {
-            from:data.email,
-            to: 'sheylapopovich@gmail.com',
-            subject: `message from $(data.name)`,
-            html:`
+  let smtpTransporter = nodemailer.createTransport({
+    service: "Gmail",
+    port: 465,
+    auth: {
+      user: "sheylapopovich@gmail.com",
+      pass: "Jamie2019!",
+    },
+  });
+  let mailOptions = {
+    from: data.email,
+    to: "sheylapopovich@gmail.com",
+    subject: `message from $(data.name)`,
+    html: `
             <h3>Information</h3>
             <ul>
             <li>Name: ${data.name}</li>
@@ -30,17 +33,18 @@ router.post('/contact', (req,res)=> {
 
             <h3>Message</h3>
             <p>${data.message}</p>
-            `
-        }
-        smtpTransporter.sendMail(mailOptions, (error)=>{
-            try {
-                if(error) return res.status(400).json({msg: 'please fill all the fields'})
-                res.status(200).json({msg: "Thank you for contacting Sheyla!"})
-            } catch (error) {
-                if(error)return res.status(500).json({msg: "There is a server error"})
-            }
-        })
-    
-})
+            `,
+  };
+  smtpTransporter.sendMail(mailOptions, (error) => {
+    try {
+      if (error)
+        return res.status(400).json({ msg: "please fill all the fields" });
+      res.status(200).json({ msg: "Thank you for contacting Sheyla!" });
+    } catch (error) {
+      if (error)
+        return res.status(500).json({ msg: "There is a server error" });
+    }
+  });
+});
 
-module.exports=router
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require("cors")
 const path = require("path");
 
+const contactRoute = require('./route/contactRoute')
 
 const app = express();
 
