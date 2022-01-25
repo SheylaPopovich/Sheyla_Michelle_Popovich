c2af4fa3566e4562476b464723d161534f853df3
Sheyla <Sheylapopovich@gmail.com>
2022-01-25 14:25:08 -0700

    added res sendfile path resolve to server js

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
-if(process.env.Node.ENV)
+if(process.env.Node_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req, res)=>(
+        res.sendFile(path.resolve(__dirname,"client", "build", "index.html"))
+    ))
+}
 
 
 const port = process.env.PORT || 5000;
