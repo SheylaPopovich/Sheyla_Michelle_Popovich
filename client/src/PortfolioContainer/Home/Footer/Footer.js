f85f82047930c53ab250a54ef22b68ed6789e4e3
Sheyla <Sheylapopovich@gmail.com>
2022-01-27 09:32:09 -0700

    removed footer

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-parent">
-        <img
+        {/* <img
           src={require("../../../assets/Home/shape-bg.png")}
           alt="Your Browser doesn't supports SVG"
-        />
+        /> */}
       </div>
     </div>
   );
