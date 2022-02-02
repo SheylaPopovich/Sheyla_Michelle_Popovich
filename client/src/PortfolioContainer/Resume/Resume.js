28ebaebd1cba6357608e4a271237a3219fa3b452
Sheyla <Sheylapopovich@gmail.com>
2022-02-02 16:01:58 -0700

    removed projects label on resumeBullets on resume js file

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -53,7 +53,6 @@ export default function Resume(props) {
     { label: "Education", logoSrc: "education.svg" },
     { label: "Work History", logoSrc: "work-history.svg" },
     { label: "Programming Skills", logoSrc: "programming-skills.svg" },
-    // { label: "Projects", logoSrc: "projects.svg" },
     { label: "Interests", logoSrc: "interests.svg" },
   ];
 
